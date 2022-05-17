34494f946b5d44453305c09e7c6f4ed523de3f60
Rahul Tamta <95517475+Rahultamta71@users.noreply.github.com>
2022-05-17 22:24:17 +0530

    Add files via upload

---
+++ Assignment_1.sql
@@ -0,0 +1,12 @@
+---- Question 1
+select FIRST_NAME as WORKER_NAME from worker;
+
+---- Question 2
+select distinct DEPARTMENT from Worker;
+
+---- Question 3
+select * from worker order by id desc limit 5;
+select * from adventure
+
+
+
